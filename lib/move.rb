2a279783ce3bed8a2bce020c0f886d72de615072
Jason Caudill <caudill.jason@gmail.com>
2019-03-22 03:20:23 +0000

    Automatically backed up by Learn

--- lib/move.rb
+++ lib/move.rb
@@ -11,6 +11,6 @@ def input_to_index(user_input)
   new_user_input = user_input.to_i
 new_user_input += -1
 end
-def move(array, index, value = "X")
+def move(board, index, character = "X")
   array[index] = value
 end
